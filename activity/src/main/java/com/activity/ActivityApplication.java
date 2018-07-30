a9e9582155984a31e5cad45ff852693654220dcc
yangyaofei <yyf@xianjinxia.com>
2018-07-30 09:59:17 +0800

    修改EurekaServer配置文件

---
+++ activity/src/main/java/com/activity/ActivityApplication.java
@@ -0,0 +1,12 @@
+package com.activity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivityApplication.class, args);
+	}
+}
